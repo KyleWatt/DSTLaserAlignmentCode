d0e10a066459de70dfd9a104b32aaba3b2630f8e
Kyle Watt <wattkjw@gmail.com>
2025-05-05 18:33:44 +0100

    updated stepper code

--- src/Motor/Stepper.cpp
+++ src/Motor/Stepper.cpp
@@ -9,107 +9,172 @@
 #include "pico/multicore.h"
 
 // Global variables
-static motor_t optic1x_motor;
-static motor_t optic1y_motor;
-static motor_t optic1z_motor;
-static motor_t optic2x_motor;
-static motor_t optic2y_motor;
-static motor_t optic2z_motor;
-static motor_t optic3x_motor;
-static motor_t optic3y_motor;
-static motor_t optic3z_motor;
-static motor_t optic4x_motor;
-static motor_t optic4y_motor;
-static motor_t optic4z_motor;
+static optic_t opticA;
+static optic_t opticB;
+static optic_t opticC;
+static optic_t opticD;
+static optic_t optics[] = {opticA, opticB, opticC, opticD};
 
-void stepper_motor_init(motor_t* motor,optic_axis_t axis){
+void stepper_motor_init(optic_t* optic,optic_select_t axis){
     // Setup the motor struct
     switch (axis)
     {
-    //OPTIC 1 ---------------------------
-    case OPTIC_1_X:
-        motor->pin_step      = STEP1_PIN;
-        motor->pin_dir       = DIR1_PIN;
-        break;
+    case OPTIC_A:
+        //OPTIC A ---------------------------
+        //Setting X motor
+        optic->motor_X.pin_step      = STEP1_PIN;
+        optic->motor_X.pin_dir       = DIR1_PIN;
+
+        //Setting Y motor
+        optic->motor_Y.pin_step      = STEP2_PIN;
+        optic->motor_Y.pin_dir       = DIR2_PIN;
     
-    case OPTIC_1_Y:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-        
-    case OPTIC_1_Z:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-        
-    //OPTIC 2 ---------------------------
-    case OPTIC_2_X:
-        motor->pin_step      = STEP1_PIN;
-        motor->pin_dir       = DIR1_PIN;
-        break;
-    
-    case OPTIC_2_Y:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-        
-    case OPTIC_2_Z:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
+        //Setting Z motor
+        optic->motor_Z.pin_step      = STEP3_PIN;
+        optic->motor_Z.pin_dir       = DIR3_PIN;
         break;
 
+    case OPTIC_B:
+        //OPTIC B ---------------------------
+        //Setting X motor
+        optic->motor_X.pin_step      = STEP4_PIN;
+        optic->motor_X.pin_dir       = DIR4_PIN;
+
+        //Setting Y motor
+        optic->motor_Y.pin_step      = STEP5_PIN;
+        optic->motor_Y.pin_dir       = DIR5_PIN;
+    
+        //Setting Z motor
+        optic->motor_Z.pin_step      = STEP6_PIN;
+        optic->motor_Z.pin_dir       = DIR6_PIN;
+        break;
+    
+    case OPTIC_C:
+        //OPTIC C ---------------------------
+        //Setting X motor
+        optic->motor_X.pin_step      = STEP7_PIN;
+        optic->motor_X.pin_dir       = DIR7_PIN;
+
+        //Setting Y motor
+        optic->motor_Y.pin_step      = STEP8_PIN;
+        optic->motor_Y.pin_dir       = DIR8_PIN;
+    
+        //Setting Z motor
+        optic->motor_Z.pin_step      = STEP9_PIN;
+        optic->motor_Z.pin_dir       = DIR9_PIN;
+        break;
+
+    case OPTIC_D:
+        //OPTIC D ---------------------------
+        //Setting X motor
+        optic->motor_X.pin_step      = STEP10_PIN;
+        optic->motor_X.pin_dir       = DIR10_PIN;
+
+        //Setting Y motor
+        optic->motor_Y.pin_step      = STEP11_PIN;
+        optic->motor_Y.pin_dir       = DIR11_PIN;
+    
+        //Setting Z motor
+        optic->motor_Z.pin_step      = STEP12_PIN;
+        optic->motor_Z.pin_dir       = DIR12_PIN;
+        break;
     }
 
-    //OPTIC 3 ---------------------------
-    case OPTIC_3_X:
-        motor->pin_step      = STEP1_PIN;
-        motor->pin_dir       = DIR1_PIN;
-        break;
-    
-    case OPTIC_3_Y:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-        
-    case OPTIC_3_Z:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-
-    //OPTIC 4 ---------------------------
-    case OPTIC_4_X:
-        motor->pin_step      = STEP1_PIN;
-        motor->pin_dir       = DIR1_PIN;
-        break;
-
-    case OPTIC_4_Y:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
-        
-    case OPTIC_4_Z:
-        motor->pin_step      = STEP2_PIN;
-        motor->pin_dir       = DIR2_PIN;
-        break;
 
     // Init motor variables
-    motor->direction = 1;
-    motor->location = 0;
-    motor->target = 0;
-    motor->enabled = false;
-    motor->next_step_time = 0;
-    motor->step_time = 250;
-    motor->target_speed = MAX_SPEED;
+    optic->motor_X.direction = 0;
+    optic->motor_Y.direction = 0;
+    optic->motor_Z.direction = 0;
+    optic->motor_X.cur_steps = 0;
+    optic->motor_Y.cur_steps = 0;
+    optic->motor_Z.cur_steps = 0;
+    optic->motor_X.cur_steps = 0;
+    optic->motor_Y.cur_steps = 0;
+    optic->motor_Z.cur_steps = 0;
+    optic->pin_enable = ENABLE_N;
+    optic->moving = false;
 
     // Initialise the GPIO
-    gpio_init(motor->pin_dir);
-    gpio_init(motor->pin_step);
-    gpio_init(motor->pin_enable);
+    gpio_init(optic->motor_X.pin_dir);
+    gpio_init(optic->motor_Y.pin_dir);
+    gpio_init(optic->motor_Z.pin_dir);
+    gpio_init(optic->motor_X.pin_step);
+    gpio_init(optic->motor_Y.pin_step);
+    gpio_init(optic->motor_Z.pin_step);
+    gpio_init(optic->pin_enable);
     
-    gpio_set_dir(motor->pin_dir, GPIO_OUT);
-    gpio_set_dir(motor->pin_step, GPIO_OUT);
-    gpio_set_dir(motor->pin_enable, GPIO_OUT);
+    gpio_set_dir(optic->motor_X.pin_dir, GPIO_OUT);
+    gpio_set_dir(optic->motor_Y.pin_dir, GPIO_OUT);
+    gpio_set_dir(optic->motor_Z.pin_dir, GPIO_OUT);
+    gpio_set_dir(optic->motor_X.pin_step, GPIO_OUT);
+    gpio_set_dir(optic->motor_Y.pin_step, GPIO_OUT);
+    gpio_set_dir(optic->motor_Z.pin_step, GPIO_OUT);
+    gpio_set_dir(optic->pin_enable, GPIO_OUT);
+}
 
-    // Init the mutex
-    mutex_init(&motor->lock);
+void enable_motors(optic_t* optic) {
+    gpio_put(optic->pin_enable, 0);
+}
+
+void disable_motor(optic_t* optic) {
+    gpio_put(optic->pin_enable, 1);
+}
+
+void set_direction(){
+    for (uint i = 0; i < 4; i++){
+        if (optics[i].motor_X.direction, optics[i].motor_X.pin_dir){
+            gpio_put(optics[i].motor_X.pin_dir, optics[i].motor_X.direction);
+        }
+        if (optics[i].motor_Y.direction, optics[i].motor_Y.pin_dir){
+            gpio_put(optics[i].motor_Y.pin_dir, optics[i].motor_Y.direction);
+        }
+        if (optics[i].motor_Z.direction, optics[i].motor_Z.pin_dir){
+            gpio_put(optics[i].motor_Z.pin_dir, optics[i].motor_Z.direction);
+        }
+    }
+    sleep_us(1);
+}
+
+void set_low(){
+    for (uint i = 0; i < 4; i++){
+        gpio_put(optics[i].motor_X.pin_step, 0);
+        gpio_put(optics[i].motor_Y.pin_step, 0);
+        gpio_put(optics[i].motor_Z.pin_step, 0);
+    }
+
+}
+
+void motors_move(){
+    while (optics[0].moving || optics[1].moving || optics[2].moving || optics[3].moving) {
+        // Set all step pins high for motors that need to move
+        for (uint i = 0; i < 4; i++){
+            if (optics[i].motor_X.cur_steps != optics[i].motor_X.target_steps){
+                gpio_put(optics[i].motor_X.pin_step, 1);
+                optics[i].motor_X.cur_steps++;
+            } else {
+                optics[i].motor_X.moving = false;
+            }
+
+            if (optics[i].motor_Y.cur_steps != optics[i].motor_Y.target_steps){
+                gpio_put(optics[i].motor_Y.pin_step, 1);
+                optics[i].motor_Y.cur_steps++;
+            } else {
+                optics[i].motor_Y.moving = false;
+            }
+
+            if (optics[i].motor_Z.cur_steps != optics[i].motor_Z.target_steps){
+                gpio_put(optics[i].motor_Z.pin_step, 1);
+                optics[i].motor_Z.cur_steps++;
+            } else {
+                optics[i].motor_Z.moving = false;
+            }
+
+            optics[i].moving = (optics[i].motor_X.moving || optics[i].motor_Y.moving || optics[i].motor_Z.moving);
+        }
+
+        // Hold step state before resetting
+        sleep_us(3); 
+        set_low(); // Set pins low after step pulse
+        sleep_us(3); // Small delay before next iteration
+    }
 }
---
+++ src/Motor/Stepper.hpp
@@ -0,0 +1,82 @@
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "pico/multicore.h"
+
+//Motor Step and DIR PINS
+
+#define STEP1_PIN 1
+#define DIR1_PIN 2
+#define STEP2_PIN 3
+#define DIR2_PIN 4
+#define STEP3_PIN 5
+#define DIR3_PIN 6
+#define STEP4_PIN 7
+#define DIR4_PIN 8
+#define STEP5_PIN 9
+#define DIR5_PIN 10
+#define STEP6_PIN 11
+#define DIR6_PIN 12
+#define STEP7_PIN 13
+#define DIR7_PIN 14
+#define STEP8_PIN 15
+#define DIR8_PIN 16
+#define STEP9_PIN 17
+#define DIR9_PIN 18
+#define STEP10_PIN 19
+#define DIR10_PIN 20
+#define STEP11_PIN 21
+#define DIR11_PIN 22
+#define STEP12_PIN 23
+#define DIR12_PIN 24
+
+// Enable Pin
+#define ENABLE_N 38
+
+typedef enum {
+    MOVING,
+    STOPPED
+} motor_state_t;
+
+typedef struct {
+    // Pins
+    uint pin_dir;
+    uint pin_step;
+
+    // Motion
+    uint cur_steps;
+    uint target_steps;
+    int8_t direction;
+
+    bool moving; 
+} motor_t;
+
+typedef struct {
+    motor_t motor_X;
+    motor_t motor_Y;
+    motor_t motor_Z;
+    uint pin_enable;
+    bool moving;
+
+} optic_t;
+
+typedef enum {
+    OPTIC_A,
+    OPTIC_B,
+    OPTIC_C,
+    OPTIC_D,
+} optic_select_t;
+
+void stepper_motor_init(motor_t* motor,optic_select_t axis);
+
+void disable_motor(motor_t* motor);
+
+void enable_motor(motor_t* motor);
+
+void motors_move();
+
+void set_direction();
+
+
+
+
